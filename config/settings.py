aa4125a96e56a195dd94aa0b9c593f411241bfea
Justinedaniellemballa839-beep <justinedaniellemballa839@gmail.com>
2026-09-22 07:06:24 +0100

    Change admin URL for security

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,6 @@ AUTH_USER_MODEL = 'comptes.Utilisateur'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
