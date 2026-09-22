d1870b619e97b9643f892f5e2421efec407fd1b3
Justinedaniellemballa839-beep <justinedaniellemballa839@gmail.com>
2026-09-22 07:16:21 +0100

    Change admin URL for security

--- config/urls.py
+++ config/urls.py
@@ -15,4 +15,4 @@ urlpatterns = [
     path('notifications/', include('notifications.urls')),
 ]
 
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
